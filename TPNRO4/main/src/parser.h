f387e16965444265b450cfdc1b3832aa2f9644af
NahuelBarrios <retro_io@hotmail.com>
2020-06-27 18:07:33 -0300

    Trabajo practico Nro4 Con Parcial incluido

---
+++ TPNRO4/main/src/parser.h
@@ -0,0 +1,14 @@
+/*
+ * parser.h
+ *
+ *  Created on: 24 jun. 2020
+ *      Author: Nahu_
+ */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+int parser_loadData(FILE* pFile , LinkedList* pArrayListPelicula);
+
+
+#endif /* PARSER_H_ */
